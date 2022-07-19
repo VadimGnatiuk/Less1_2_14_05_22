ebfd1cd62e0a188917289391ba864975b5a7ce70
Gnatiuk_VV <krijanovka_nvk@gmail.com>
2022-07-19 22:34:57 +0300

    Less1_2

---
+++ 14-05-2022_001.py
@@ -0,0 +1,22 @@
+print('Hello World!')
+print("My name - Vadim.")
+print("Wiskey Jack Danies's")
+print("Wiskey Jack Danies\'s")
+name = 'Vadim'
+surname = 'Gnatiuk'
+age = 17
+print(name,surname,age)
+print(name,surname,age,sep='***')
+print(name,surname,age,sep='___',end=' ')
+print(name,surname,age,sep='---')
+#str string строка 'Вадим'
+#int integer целое число 47
+#float float число с плавающей точкой (дробоное число) 5.5
+#bool bolean логический тип True False логическое значение
+#Ввод с клавиатуры
+name = input('Сейчас переменная name - это мое имя. Введите имя: ')
+print(type(name))
+age = 5.5
+print(type(age))
+name = int(input('Сейчас переменная name - это целое число Enter name : '))
+print(type(name))
---
+++ 14-05-2022_less2.py
@@ -0,0 +1,40 @@
+# name = str(input('Enter your name : '))
+# age = int(input('Enter your age : '))
+# height = float(input('Enter your height : '))
+# is_student = bool(input('Enter, you are a student : '))
+# print(name, age, height, is_student)
+# print('My name is ', name, "and my age is", age, '. My height is ', height, 'and I am students - ', is_student)
+# #Конкатенация строк:
+# print('My name is '+str(name)+"and my age is "+str(age)+'. My height is '+str(height)+' and I am students - '+str(is_student))
+# #Метод вывода основанный на "формате" (использовался ранее да версий 3.3 и 3.4.)
+# print('My name is {} and my age is {}. My height is {} and I am students - {}'.format(name,age,height,is_student))
+# print(f'My name is {name} and my age is {age}. My height is {height} and I am students - {is_student}')
+#*******************************************************************************
+number1 = 7
+number2 = 5
+print(number1 + number2)
+number3 = (number1 + number2) * number1
+print(number3)
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 // number2)
+print(number1 % number2)
+print(number1 ** number2)
+
+a = 3
+b = 4
+c = 5
+print ((a+b+c)/3)
+print (f'({a} + {b} + {c}) / 3 = {(a+b+c)/3}')
+# Площадь прямоугольника
+st1 = 10
+st2 = 7
+print(f'Площадь прямоугольника : {st1} x {st2} = {st1*st2}')
+
+
+
+
+
+
